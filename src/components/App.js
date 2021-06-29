ee8919756eedb35417777004bb4a90c2487b750f
Andrew McIndoe <amcindoe@powerhrg.com>
2021-06-29 10:18:31 -0400

    Import/Export Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+import About from "../components/About"
+import NavBar from "../components/NavBar"
+import Home from "../components/Home"
+
+
 function App() {
   return (
     <div>
